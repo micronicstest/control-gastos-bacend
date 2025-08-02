f1ead59d5aac8a3dc40ddb543187f5c232d0f01d
micronicstest <ospinad584@gmail.com>
2025-08-02 12:48:40 -0500

    config cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // --- Configuración de CORS ---
 const corsOptions = {
-  origin: "*",
+  origin: "https://control-gastos-bacend.vercel.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
